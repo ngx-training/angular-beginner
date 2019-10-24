19911c2aabab30ab1dd1b1ad7f1f3d6d17fea764
Gregor Doroschenko <gregor.doroschenko@develappers.de>
2019-10-24 09:53:13 +0200

    fix lower case upper case

--- src/app/pipes/funny.pipe.ts
+++ src/app/pipes/funny.pipe.ts
@@ -5,14 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FunnyPipe implements PipeTransform {
 
-  transform(value: string): string {
+  transform(value: string, ...argv: any[]): string {
+    console.log(argv);
     if (value) {
       const strArr = [...value];
       strArr.forEach((letter, index) => {
         if (index % 2 === 0) {
-          strArr[index] = letter.toUpperCase();
+          strArr[index] = letter.toLowerCase();
         } else {
-          strArr[index] = letter;
+          strArr[index] = letter.toUpperCase();
         }
       });
       value = strArr.join('');
